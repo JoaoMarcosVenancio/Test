296e78f5dddd9adf473bd0fbdd5d0d0202a1d37d
Joao Oliveira <joaomarcos.oliveiracontato@gmail.com>
2023-09-24 17:01:52 -0300

    add gitignore and refactor main activity

--- app/src/main/java/com/example/mvdesafioa/MainActivity.kt
+++ app/src/main/java/com/example/mvdesafioa/MainActivity.kt
@@ -1,25 +1,16 @@
 package com.example.mvdesafioa
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mvdesafioa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setNavStartDestination()
-    }
-
-    private fun setNavStartDestination() {
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.navHostContainer) as NavHostFragment
-        val navController = navHostFragment.navController
     }
 }
